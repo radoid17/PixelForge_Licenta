b813686ce4176d954be59a37ef7e8583b1d5cc1f
DRAGOS COSMIN RADOI <dragos-cosmin.radoi@s.unibuc.ro>
2025-08-09 17:32:54 +0300

    Fixes

--- PixelForge/Models/ReviewVote.cs
+++ PixelForge/Models/ReviewVote.cs
@@ -7,12 +7,9 @@ namespace PixelForge.Models
     {
         [Required]
         public string UserId { get; set; }
-
         [Required]
         public int ReviewId { get; set; }
-
         public bool IsLike { get; set; }
-
         public PixelForgeUser User { get; set; }
         public Review Review { get; set; }
     }
